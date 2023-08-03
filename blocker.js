3286757071204f4e0a0af80161f1cccf66b844a8
Anchit Singh <58829755+AnchitSingh@users.noreply.github.com>
2023-08-03 14:45:32 +0530

    Create blocker.js

---
+++ blocker.js
@@ -0,0 +1,24 @@
+hideDivs();
+
+// Observer to detect added nodes
+const observer = new MutationObserver(hideDivs);
+
+// Run on childList changes under parent element  
+observer.observe(parentElem, {
+    childList: true
+});
+
+function hideDivs() {
+  
+    const cellInnerDivs = document.querySelectorAll('div[data-testid="cellInnerDiv"]');
+
+    // Loop through each div 
+    cellInnerDivs.forEach(div => {
+        // Check if it contains a child div with the specific class and data attribute
+        if (div.querySelector('div.css-1dbjc4n[data-testid="placementTracking"]')) {
+            // If yes, set display to none
+            div.style.display = 'none';
+        }
+
+    });
+}
