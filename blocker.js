acdb6b8e58c209292a45a4d3876647aa52fe8dcf
Anchit Singh <58829755+AnchitSingh@users.noreply.github.com>
2023-08-03 15:25:53 +0530

    Fixed Ad detection algorithm

--- blocker.js
+++ blocker.js
@@ -1,24 +1,47 @@
-hideDivs();
+var adCount = 0;
+function hasChildWithParameters(div) {
+    const expectedDataTestIds = [
+        'top-impression-pixel',
+        'right-impression-pixel',
+        'bottom-impression-pixel',
+        'left-impression-pixel',
+    ];
 
-// Observer to detect added nodes
-const observer = new MutationObserver(hideDivs);
+    const childDivs = div.querySelectorAll('div.css-1dbjc4n[data-testid="placementTracking"]');
+    if (childDivs.length === 0) {
+        return false;
+    }
 
-// Run on childList changes under parent element  
-observer.observe(document, {
-    childList: true
-});
+    // Check if the child div contains four divs with the expected data-testid values
+    const childDivChildren = childDivs[0].querySelectorAll('div[data-testid]');
+    const actualDataTestIds = Array.from(childDivChildren).map((child) => child.getAttribute('data-testid'));
 
-function hideDivs() {
-  
+    return expectedDataTestIds.every((dataTestId) => actualDataTestIds.includes(dataTestId));
+}
+
+// Function to set the display property of divs with the specified parameters
+function hideDivsWithChildParameters() {
     const cellInnerDivs = document.querySelectorAll('div[data-testid="cellInnerDiv"]');
-
-    // Loop through each div 
-    cellInnerDivs.forEach(div => {
-        // Check if it contains a child div with the specific class and data attribute
-        if (div.querySelector('div.css-1dbjc4n[data-testid="placementTracking"]')) {
-            // If yes, set display to none
+    cellInnerDivs.forEach((div) => {
+        if (hasChildWithParameters(div)) {
+        	if(div.style.display != 'none'){
+        		adCount+=1;
+        		console.log(`Ads found and hidden: ${adCount}`);
+        	}
             div.style.display = 'none';
         }
-
     });
 }
+// Initial call to hide the divs that match the criteria on page load
+hideDivsWithChildParameters();
+
+// Using MutationObserver to watch for changes in the DOM and hide new divs accordingly
+const observer = new MutationObserver(() => {
+    hideDivsWithChildParameters();
+});
+
+// Start observing changes in the entire document
+observer.observe(document, {
+    childList: true,
+    subtree: true,
+});
