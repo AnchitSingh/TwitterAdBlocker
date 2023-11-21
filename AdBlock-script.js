37be9cf826e9d645387d09d51bd98e8305f5dfd8
Anchit Singh <58829755+AnchitSingh@users.noreply.github.com>
2023-11-21 15:23:34 +0530

    Update and rename blocker.js to AdBlock-script.js

--- AdBlock-script.js
+++ AdBlock-script.js
@@ -7,7 +7,7 @@ function hasChildWithParameters(div) {
         'left-impression-pixel',
     ];
 
-    const childDivs = div.querySelectorAll('div.css-1dbjc4n[data-testid="placementTracking"]');
+    const childDivs = div.querySelectorAll('div[data-testid="placementTracking"]');
     if (childDivs.length === 0) {
         return false;
     }
@@ -19,15 +19,21 @@ function hasChildWithParameters(div) {
     return expectedDataTestIds.every((dataTestId) => actualDataTestIds.includes(dataTestId));
 }
 
+// Function to update the ad count in the popup
+function updateAdCount(count) {
+    console.log(count)
+}
+
+
 // Function to set the display property of divs with the specified parameters
 function hideDivsWithChildParameters() {
     const cellInnerDivs = document.querySelectorAll('div[data-testid="cellInnerDiv"]');
     cellInnerDivs.forEach((div) => {
         if (hasChildWithParameters(div)) {
-        	if(div.style.display != 'none'){
-        		adCount+=1;
-        		console.log(`Ads found and hidden: ${adCount}`);
-        	}
+            if (div.style.display != 'none') {
+                adCount += 1;
+                updateAdCount(adCount);
+            }
             div.style.display = 'none';
         }
     });
